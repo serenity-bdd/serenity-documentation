98bf58799093dd325b7507680d8776ebbbf47b07
YamStranger <YamStranger@bk.ru>
2015-12-03 23:06:39 +0000

    35_issue: added examples, added report screenshot

---
+++ src/samples/junit-tests-linked-to-issues/src/test/java/net/serenity/samples/retries/TestSteps.java
@@ -0,0 +1,34 @@
+package net.serenity.samples.retries;
+
+import net.thucydides.core.annotations.Step;
+import org.junit.Assert;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TestSteps {
+
+    @Step
+    public void initialization() {
+        action();
+    }
+
+    @Step
+    public void when_example_action_for(final int value) {
+        action();
+        Assert.assertTrue(true);
+    }
+
+    @Step
+    public void then_example_result_should_be(final int value) {
+        action();
+        Assert.assertTrue(true);
+    }
+
+    private void action() {
+        try {
+            Thread.sleep(ThreadLocalRandom.current().nextInt(50, 200));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
